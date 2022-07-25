6164f9595c0c4ed80803ce3ac52ffd357ba9bd55
doc <dmchugaev@gmail.com>
2022-07-25 11:02:44 +0700

    upd classes

--- src/menu/constants.ts
+++ src/menu/constants.ts
@@ -1,4 +1,4 @@
-export const GAME_IS_STARTED_CLASS = 'page--is-started-game';
+export const GAME_IS_STARTED_CLASS = 'game--is-started';
 
 export const PAGE_ID = 'page';
 export const GAME_CONTAINER_ID = 'game-container';
